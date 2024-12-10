e7b98d5315bf2bdd70266f3c9be595d294bf8a29
Sam Detnon <samueldetnon@gmail.com>
2024-12-10 11:31:05 +0000

    Code currently compared line by line, which is wrong! TODO: create two lists on the string split, order them and then do the comparison

--- src/day_1.rs
+++ src/day_1.rs
@@ -2,15 +2,33 @@ use std::fs::read_to_string;
 use std::path::Path;
 
 fn main() {
-    get_total_distance();
+   get_total_distance();
 }
 
-fn get_total_distance(){
-    let path = Path::new("./inputs/day_1.txt");
+fn get_total_distance() -> String {
+    let path = Path::new("../inputs/day_1.txt");
     let _display = path.display();
     
-    let _inputs: Vec<String> = read_input(path);
+    let inputs: Vec<String> = read_input(path);
 
+
+    let mut distance: i32 = 0;
+    // Iterate line by line, remove the whitespace and return the difference between the two values
+    for i in inputs{
+        let mut nums = i.split_whitespace();
+        let first: i32 = nums.next().unwrap().parse().expect("This should be a number!");
+        let second: i32 = nums.next().unwrap().parse().expect("This should be a number!");
+       
+
+
+        if first > second {
+            distance = distance + (first - second)
+        } else {
+            distance = distance + (second - first)    
+        };
+    }
+    println!("{}", distance);
+    distance.to_string()
 }
 
 
